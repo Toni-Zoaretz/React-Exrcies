0036d36a3b14994dd83bb408656cc9f5848fcd5f
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-15 20:34:34 +0200

    6.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  function color() {
+    if (counter === 0) {
+      return "black";
+    }
+    if (counter < 0) {
+      return "red";
+    }
+    if (counter > 0) {
+      return "green";
+    }
+  }
+  const [counter, setCounter] = useState(0);
+
+  const incHandler = () => {
+    if (counter < 10) {
+      setCounter(counter + 1);
+    }
+  };
+  const decHandler = () => {
+    if (counter > -10) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <button onClick={incHandler}>+</button>
+      <button onClick={decHandler}>-</button>
+      <div className={color()}>
+        <span>{counter}</span>
+      </div>
     </div>
   );
 }
 
 export default App;
+// We want to create two buttons and one label.
+// The first button will increment the counter.
+// The second button will decrement the counter
+// A label that displays the current value.
+// The counter range can only be from -10 to 10.
+// If the counter is negative the label will be a red color.
+// If the counter is positive the label will be a green color.
+// If the counter is 0 the label will be black in color.
