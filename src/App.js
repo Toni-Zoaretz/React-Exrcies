695a4c9855a6c37adcdde51adb99f30a63fb17b1
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-20 15:15:19 +0200

    9.1_react_data_massaging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { data } from "./data";
 function App() {
+  const newDate = data.map((name) => name.name);
+  const bornBefore1990 = data.filter((birthYear) => {
+    return Number(birthYear.birthday.slice(-4)) < 1990;
+  });
+  console.log(bornBefore1990);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hey!!</h1>
+      {newDate.map((name, index) => {
+        return <p key={index}>hello {name}</p>;
+      })}
+      {bornBefore1990.map((birthday, i) => {
+        return (
+          <p key={i}>
+            here are the people borne before 1990: {birthday.name}
+            {birthday.birthday} ,
+            {birthday.favoriteFoods.fish.map((fish) => (
+              <p>{fish}</p>
+            ))}
+            ,
+            {birthday.favoriteFoods.meats.map((meat) => (
+              <p>{meat}</p>
+            ))}
+          </p>
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+// 3. Save the modified data to the state.
+// 4. Create a Name component that you will pass via props the
+// names and print them on the screen.
+// 5. Create a Card component that you will pass via props the
+// objects before 1990 and print them on the screen.
+// - which includes their name, birthday, favorite meats,
+// and favorite fish foods.
