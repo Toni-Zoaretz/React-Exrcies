d99def07000d63ba95f2fa4e30957855dca47714
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-21 19:52:21 +0200

    13.3_changing_box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [color, setColor] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const colors = ["box1", "box2", "box3", "box4", "box5"];
+    setTimeout(() => {
+      setColor(colors[index]);
+      setIndex(index + 1);
+      if (index === 5) {
+        setIndex(0);
+      }
+    }, 500);
+  }, [color, index]);
+  return <div className={`box ${color}`}></div>;
 }
 
 export default App;
+
+// 1. We want to create a simple app: a box that changes color
+// every half a second.
+// 2. Let’s change the box to a circle after 5 color changes
